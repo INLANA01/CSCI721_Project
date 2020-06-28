6e3523a24406cb21cf90d96eaf65f4d8bd5ac6c7
Aditya Avinash Landge <al2960@ad.rit.edu>
2020-06-28 14:27:55 -0400

    Update queries

--- SQL_Queries_to_make_2_databases_uniform.sql
+++ SQL_Queries_to_make_2_databases_uniform.sql
@@ -17,19 +17,19 @@ from ChadDataset
 
 #--------------------------------------------------Chad Dataset----------------------------------------------------#
 Select playerID, playerName, Season, teamID, League, G, AB, R, H, 2B, 3B, HR, RBI, SB, BB, SO, IBB, HBP, SH, SF, salary from ChadDatasetTable
-GROUP BY playerName, Season
-having
+where
 R != 0
 and G > 10
 and SF != 0
-and season >= 2000
+and 
+season >= 1990;
 order by season desc;
 #---------------------------------------------MLB Dataset-------------------------------------------------#
 Select playerID, `Player Name` as playerName,  Season, teamID, League, G, AB, R, H, 2B, 3B, HR, RBI, SB, BB, SO, IBB, HBP, SH, SF, salary from mlb_batting
-GROUP BY playerName, Season
-having
+where
 R != 0
 and G > 10
 and SF != 0
-and season >= 2000 
+and 
+season >= 1990;
 order by season desc;
